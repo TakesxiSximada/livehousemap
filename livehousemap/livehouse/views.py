5d2e9bdb40ed901b106360ac9f900f39fd7f6bc1
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-01-19 18:45:29 +0900

    dbからjsonを出力するように修正

--- livehousemap/livehouse/views.py
+++ livehousemap/livehouse/views.py
@@ -1,5 +1,25 @@
 # Create your views here.
+import json
 from django.http import HttpResponse
+from .models import (
+    House,
+    Landmark,
+    )
 
 def index(request):
-    return HttpResponse('{"houses": [{"lat": 11.111, "lng": 99.999, "name": "testname", "landmarks": []}]}', mimetype='application/json')
+    houses = []
+    for house in House.objects.all():
+        data = {'name': house.name,
+                'lat': house.lat,
+                'lng': house.lon,
+                }
+        landmarks = []
+        for landmark in Landmark.objects.filter(id=house.id):
+            landmark = {'name': landmark.name,
+                        'lat': landmark.lat,
+                        'lng': landmark.lng,
+                        }
+            landmarks.append(landmark)
+        houses.append(data)
+    json_str = json.dumps(houses)
+    return HttpResponse(json_str, mimetype='application/json')
