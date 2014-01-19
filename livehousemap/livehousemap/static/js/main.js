4ec02a03ecb6864fe71e5147810a65241240d915
foo9 <fu9fu9fu9@gmail.com>
2014-01-19 17:38:57 +0900

    Add Google Map

--- livehousemap/livehousemap/static/js/main.js
+++ livehousemap/livehousemap/static/js/main.js
@@ -1,1 +1,14 @@
 
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+
+$(function() {
+  initialize();
+});
